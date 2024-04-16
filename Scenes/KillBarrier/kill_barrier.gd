953ddfb7a041563dbb85434b5b6748da2ea2ee80
Crosgaard <conrad@therosgaards.dk>
2024-04-16 19:59:09 +0200

    #12

--- Scenes/KillBarrier/kill_barrier.gd
+++ Scenes/KillBarrier/kill_barrier.gd
@@ -1,12 +1,14 @@
 extends Area2D
 
+@export var start_point: Vector2
+@export var end_point: Vector2
 @export var time: float
 
-var direction: Vector2
-var distance: Vector2
+func _ready():
+	position = start_point
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", end_point, time)
 
-@onready var start_point: Vector2 = %StartPoint.global_position
-@onready var end_point: Vector2 = %EndPoint.global_position
-
-func _ready() -> void:
-	
+func _on_body_entered(body):
+	if body is Player:
+		body.die()
