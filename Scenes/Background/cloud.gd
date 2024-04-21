8c47b8cc05e4b73bd7a434a1c2201243db4af155
Crosgaard <conrad@therosgaards.dk>
2024-04-21 21:00:20 +0200

    Tutorial level done

--- Scenes/Background/cloud.gd
+++ Scenes/Background/cloud.gd
@@ -1,6 +1,6 @@
 extends ParallaxLayer
 
-@export var cloud_speed: float = 25
+@export var cloud_speed: float = 50
 
 func _process(delta: float) -> void:
 	self.motion_offset.x += cloud_speed * delta
--- Scenes/Levels/level_parent.gd
+++ Scenes/Levels/level_parent.gd
@@ -5,7 +5,7 @@ class_name LevelParent extends Node2D
 func enter_dimension() -> void:
 	visible = true
 	%ParallaxBackground.visible = true
-	tileMap.set_layer_enabled(0, true)
+	tileMap.set_layer_enabled(1, true)
 	for child in $KillAreas.get_children():
 		if "set_disabled" in child:
 			child.set_disabled(false)
@@ -13,7 +13,7 @@ func enter_dimension() -> void:
 func exit_dimension() -> void:
 	visible = false
 	%ParallaxBackground.visible = false
-	tileMap.set_layer_enabled(0, false)
+	tileMap.set_layer_enabled(1, false)
 	for child in $KillAreas.get_children():
 		if "set_disabled" in child:
 			child.set_disabled(true)
